bdc463d762156cd04dab991c9bfacc33c42632cc
francisricleruiz@gmail.com <francisricleruiz@gmail.com>
2024-02-07 17:34:48 -0300

    1.1.0

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -1,7 +1,7 @@
 require("utils")
 local linters = {
 	"eslint_d",
-	"fish",
+	"pylint",
 }
 local formatters = {
 	"prettier",
@@ -11,6 +11,37 @@ local formatters = {
 	"black",
 }
 return {
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			local highlight = {
+				"RainbowRed",
+				"RainbowYellow",
+				"RainbowBlue",
+				"RainbowOrange",
+				"RainbowGreen",
+				"RainbowViolet",
+				"RainbowCyan",
+			}
+
+			local hooks = require("ibl.hooks")
+			-- create the highlight groups in the highlight setup hook, so they are reset
+			-- every time the colorscheme changes
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+			end)
+
+			require("ibl").setup({ indent = { highlight = highlight } })
+		end,
+	},
+	-- FORMATTER
 	{
 		"stevearc/conform.nvim",
 		config = function()
@@ -27,6 +58,7 @@ return {
 			})
 		end,
 	},
+	-- AUTOINSTALL LINTERS AND FORMATTRS
 	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		config = function()
@@ -35,15 +67,17 @@ return {
 			})
 		end,
 	},
+	-- LINTER
 	{
 		"mfussenegger/nvim-lint",
+		events = { "BufReadPre", "BufNewFile" },
 		opts = {
-			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+			events = { "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" },
 			linters_by_ft = {
-				fish = { "fish" },
+				astro = { "eslint_d" },
+				python = { "pylint" },
 				javascript = { "eslint_d" },
 				typescript = { "eslint_d" },
-				astro = { "eslint_d" },
 				javascriptreact = { "eslint_d" },
 				typescriptreact = { "eslint_d" },
 			},
@@ -52,11 +86,16 @@ return {
 			local lint = require("lint")
 			lint.linters_by_ft = opts.linters_by_ft
 			lint.linters = linters
+			local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
 			vim.api.nvim_create_autocmd(opts.events, {
+				group = lint_autogroup,
 				callback = function()
 					lint.try_lint()
 				end,
 			})
+			vim.keymap.set("n", "<leader>l", function()
+				lint.try_lint()
+			end, { desc = "Trigger linting for current file" })
 		end,
 	},
 }
