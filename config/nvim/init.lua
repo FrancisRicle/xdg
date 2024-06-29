ad5c2291b20b37460fad590271e307eb87bd29bb
Francis Ricle <francisricleruiz@gmail.com>
2024-06-29 07:11:50 -0300

    FIX nvim config add fonts, wallpapers and dzconky

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,9 +3,7 @@ vim.keymap.set({ "n", "i", "t" }, "<C-S>", "<cmd>wa<cr>")
 vim.keymap.set({ "n", "i", "t" }, "<C-E>", "<cmd>Telescope file_browser<cr>")
 vim.keymap.set({ "n", "i", "t" }, "<C-B>", "<cmd>Telescope buffers<cr>")
 vim.keymap.set({ "n", "i", "t" }, "<C-T>", "<cmd>Telescope toggleterm_manager<cr>")
-vim.o.expantab = true
 vim.o.expandtab = true
-vim.o.autoident = true
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
